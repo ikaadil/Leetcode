5c2b7a086a957aeda9d8cc7765b35205aafa5aa7
Ifta khairul Alam Adil <ikaadil007@gmail.com>
2023-03-28 16:07:29 +0600

    TC: O(n ^ 2) SC: O(n ^ 2)

---
+++ 64-minimum-path-sum/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int row = grid.size(), column = grid[0].size();
+        vector<vector<int>>ans(row + 1, vector<int>(column + 1, 123456));
+        ans[0][0] = ans[0][1] = ans[1][0] = 0;
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j <= column; j++) {
+                ans[i][j] = grid[i - 1][j - 1] + min(ans[i - 1][j], ans[i][j - 1]);
+            }
+        }
+        return ans[row][column];
+    }
+};
